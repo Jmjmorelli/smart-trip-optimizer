70a5b7e568e7d1d9ef480b086dae3f8a6a47d61c
seb <sebasgonz0824@gmail.com>
2025-04-13 01:50:37 -0400

    login success/failure pretty looking

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,6 +4,8 @@ import Register from './Register';
 import Navbar from './Navbar'; 
 import Landing from './Landing';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
@@ -17,6 +19,15 @@ function App() {
           <Route path="/login" element ={<Login/>} />
           <Route path="/home" element ={<Home/>} />
         </Routes>
+        <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        theme="colored"
+      />
       </BrowserRouter>
     </div>
   )
