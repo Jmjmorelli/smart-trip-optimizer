3756c2e92a58725c2a91d0eceeab27ec00ace8c1
Jean Carlos <135998944+jawcoke@users.noreply.github.com>
2025-04-13 05:29:53 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,10 +8,7 @@ const gptRoute = require('./routes/gpt');
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 
 require('dotenv').config();
 
